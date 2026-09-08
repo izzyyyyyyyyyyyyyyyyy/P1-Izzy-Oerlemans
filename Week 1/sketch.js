a268dbad0ec6fabca7daaf4bcbcc38f680f14a99
izzyyyyyyyyyyyyyyyyy <izzy61887@gmail.com>
2026-09-08 12:42:52 +0200

    vlag en naam klaar

--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -1,7 +1,23 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1200, 800);
 }
 
 function draw() {
   background(220);
+  //volledige naam
+  textSize(32)
+  fill(0,0,0)
+  text("Izzy Oerlemans",10,30)
+
+  //vlag
+  fill(100,200,255)
+  rect(10,70,335,35)
+  fill(255,182,193)
+  rect(10,105,335,35)
+  fill(255,255,255)
+  rect(10,140,335,35)
+  fill(255,182,193)
+  rect(10,175,335,35)
+  fill(100,200,255)
+  rect(10,210,335,35)
 }
